7530751e066874374c83675794e7e104c556d000
Javier Casares <javier@casares.org>
2023-01-21 09:39:16 +0100

    inicio de CPT y plugin

---
+++ animal-shelter/animal-shelter.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Animal Shelter
+Plugin URI: https://wordpress.org/plugins/animal-shelter/
+Description: Animal Shelter plugin
+Version: 1.0.0
+Requires at least: 6.1
+Tested: 6.1
+Requires PHP: 7.4
+Tested PHP: 8.1
+Author: WordPress Granada Community
+Author URI: https://wpgranada.es/
+License: EUPL 1.2
+License URI: https://eupl.eu/1.2/en/
+Text Domain: animal-shelter
+*/
+defined( 'ABSPATH' ) or die( 'Bye bye!' );
+
+require_once( './custom-post-type/dog.php' );
