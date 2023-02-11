f551a366adbc95877ed9fcd27c8595932b502ec6
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-02-11 15:46:40 +0100

    feat: add CPT icons

--- admin/class-animalshelter-cpt-dog.php
+++ admin/class-animalshelter-cpt-dog.php
@@ -8,7 +8,7 @@ class Animalshelter_Cpt_Dog extends Animalshelter_Cpt {
 		$this->rewrite     = __( 'dogs', 'animal-shelter' );
 		$this->label       = __( 'Dog', 'animal-shelter' );
 		$this->description = __( 'Animal: Dog', 'animal-shelter' );
-		$this->menu_icon   = 'dashicons-portfolio';
+		$this->menu_icon   = 'data:image/svg+xml;base64,' . base64_encode( file_get_contents( ANIMALSHELTER_PLUGIN_ADMIN_DIR . 'img/dog.svg' ) );
 	}
 
 	public function initCPT() {
