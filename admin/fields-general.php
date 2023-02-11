fa77c274f0f4b1530111eaf8af2df6fa4cbd5f60
David Perez <david@closemarketing.es>
2023-02-11 16:13:47 +0100

    field init

--- admin/fields-general.php
+++ admin/fields-general.php
@@ -2,12 +2,8 @@
 // to be merged in class-animalshelter-cpt.php
 $this->fields = array(
 	array(
-		'single'            => true,
-		'type'              => 'string',
-		'show_in_rest'      => true,
-		'sanitize_callback' => 'sanitize_text_field',
-		'auth_callback'     => function() {
-			return current_user_can( 'edit_posts' );
-		},
+		'label' => __( 'Label', 'animal-shelter' ),
+		'key'   => 'subtitle',
+		'type'  => 'string',
 	),
 );
