bf2c437763ecfefb3776b11515b1abb3236ee838
Javier Casares <javier@casares.org>
2023-02-11 16:41:55 +0100

    Fix mutt

--- includes/breed/class-animalshelter-breed-dog.php
+++ includes/breed/class-animalshelter-breed-dog.php
@@ -9,7 +9,7 @@ class Animalshelter_Breed_Dog {
 	public function __construct() {
 		$this->breeds = array(
 			'unknown' => array ( 'name' => __( 'Unknow', 'animal-shelter' ) ),
-			'mongrel' => array ( 'name' => __( 'Mongrel', 'animal-shelter' ) ),
+			'mutt' => array ( 'name' => __( 'Mutt', 'animal-shelter' ) ),
 			'afador' => array ( 'name' => __( 'Afador', 'animal-shelter' ) ),
 			'affenhuahua' => array ( 'name' => __( 'Affenhuahua', 'animal-shelter' ) ),
 			'affenpinscher' => array ( 'name' => __( 'Affenpinscher', 'animal-shelter' ) ),
