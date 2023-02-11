d970630f00cf49dfcd566a55245b4fb1b8c54826
juanmacivico87 <juanmacivico87@gmail.com>
2023-02-11 13:33:37 +0100

    Create classes to terms to new taxonomies to breed for dogs and cats

--- public/class-animalshelter-term-breed-dog.php
+++ public/class-animalshelter-term-breed-dog.php
@@ -1,6 +1,6 @@
 <?php
 
-class Animalshelter_Term_Breed extends Animalshelter_Term {
+class Animalshelter_Term_Breed_Dog extends Animalshelter_Term {
 	public function __construct( $id = 0 ) {
 		$this->taxonomy = ANIMALSHELTER_TAXONOMY_BREED_DOG;
 		parent::__construct( $id );
