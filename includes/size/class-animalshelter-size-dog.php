61a98b97dee5cede57d1dcaac0f1b53c78065f34
Javier Casares <javier@casares.org>
2023-02-12 10:50:07 +0100

    i18l fix

--- includes/size/class-animalshelter-size-dog.php
+++ includes/size/class-animalshelter-size-dog.php
@@ -10,10 +10,10 @@ class Animalshelter_Size_Dog {
 		$this->sizes = array(
 			'unknown' => array( 'name' => __( 'Unknown', 'animal-shelter' ) ),
 			'mini' => array( 'name' => _x( 'Mini (< 3kg/6lbs)', 'Size of a dog', 'animal-shelter' ) ),
-			'small' => array( 'name' => __( 'Small (3kg/6lbs - 10kg/22lbs)', 'Size of a dog', 'animal-shelter' ) ),
-			'medium' => array( 'name' => __( 'Medium (10kg/22lbs - 25kg/55lbs)', 'Size of a dog', 'animal-shelter' ) ),
-			'large' => array( 'name' => __( 'Large (25kg/55lbs - 50kg/110lbs)', 'Size of a dog', 'animal-shelter' ) ),
-			'giant' => array( 'name' => __( 'Giant (> 50kg/110lbs)', 'Size of a dog', 'animal-shelter' ) ),
+			'small' => array( 'name' => _x( 'Small (3kg/6lbs - 10kg/22lbs)', 'Size of a dog', 'animal-shelter' ) ),
+			'medium' => array( 'name' => _x( 'Medium (10kg/22lbs - 25kg/55lbs)', 'Size of a dog', 'animal-shelter' ) ),
+			'large' => array( 'name' => _x( 'Large (25kg/55lbs - 50kg/110lbs)', 'Size of a dog', 'animal-shelter' ) ),
+			'giant' => array( 'name' => _x( 'Giant (> 50kg/110lbs)', 'Size of a dog', 'animal-shelter' ) ),
 		);
 
 	}
